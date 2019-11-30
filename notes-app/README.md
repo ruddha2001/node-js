648c6922e6c9fcb86a4d0f7a1f5703891feee6ba
ruddha2001 <ruddha.mine@gmail.com>
2019-11-30 16:31:24 +0530

    Added basic yargs commands

---
+++ notes-app/README.md
@@ -0,0 +1,11 @@
+# NOTES-APP
+
+This is basic CLI application for taking notes!
+
+The application has following four functionalities:
+* Add a note
+* Remove a note
+* Read a note
+* List all notes
+
+Instead of using a databse, this app uses a simple text file on the file system of the user to store and retrieve notes.
--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,58 @@
-const getNotes = require ('./notes');
+const fs = require ('fs');
+const chalk = require("chalk");
+const yargs = require ('yargs');
+const getNotes = require("./notes");
 
-console.log(getNotes());
+//Customize yargs version
+yargs.version('1.1.0');
+
+//Create add command
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log("Title: " + argv.title);
+        console.log("Body: " + argv.body);
+    }
+});
+
+//Create remove command
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function(){
+        console.log("Removing a note!");
+    }
+});
+
+//Create read command
+yargs.command({
+    command: "read",
+    describe: "Read a note",
+    handler: function(){
+        console.log("Reading a note!");
+    }
+});
+
+//Create list command
+yargs.command({
+    command: "list",
+    describe: "List the notes",
+    handler: function(){
+        console.log("Listing all notes!");
+    }
+});
+
+yargs.parse();
